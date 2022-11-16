3b6e62ec02f81cbe029b8efaa9c111d49baf50e4
Aqua <99206531+WedleySilva@users.noreply.github.com>
2022-11-16 11:27:31 -0300

    Add files via upload

---
+++ Correcao_banco_MUSEU.sql
@@ -0,0 +1,95 @@
+-- Geração de Modelo físico
+-- Sql ANSI 2003 - brModelo.
+CREATE DATABASE 	banco_museu;
+USE banco_museu;
+
+
+CREATE TABLE autor (
+cod_autor int PRIMARY KEY,
+nacionalidade_autor char(3) not null,
+nome_autor varchar(80) not null
+);
+
+CREATE TABLE salao (
+cod_salao int PRIMARY KEY,
+num_salao int not null,
+andar_museu int not null,
+salao varchar(80) not null
+);
+
+CREATE TABLE salao_obra (
+salao_cod_salao int not null,
+obra_cod_obra int not null,
+posicao_salao varchar(80) not null,
+FOREIGN KEY(salao_cod_salao) REFERENCES salao (cod_salao)
+);
+
+CREATE TABLE atividade (
+ob_cod_obra int not null,
+func_id_funcionario int not null,
+hora_entrada time not null,
+hora_saida time not null,
+data_atividade date not null
+);
+
+CREATE TABLE tipo_funcionario (
+cod_tipo_funcionario int PRIMARY KEY,
+tipo_funcionario varchar(80) not null
+);
+
+CREATE TABLE materia_prima (
+cod_mat_prima int PRIMARY KEY,
+qtd_est_mat int not null,
+nome_mat_prima varchar(80) not null
+);
+
+CREATE TABLE funcionario (
+id_funcionario int PRIMARY KEY,
+nome_funcionario varchar(80) not null,
+salario_funcionario decimal(10,2) not null,
+cpf_funcionario varchar(14) not null unique,
+cod_tipo_funcionario int not null,
+FOREIGN KEY(cod_tipo_funcionario) REFERENCES tipo_funcionario (cod_tipo_funcionario)
+);
+
+CREATE TABLE manutencao (
+mnt_obra int PRIMARY KEY,
+data_termi_mnt date not null,
+custo_mnt decimal(10,2) not null,
+data_ini_mnt date not null,
+desc_mnt varchar(80) not null,
+cod_obra int not null,
+func_id_funcionario int not null,
+FOREIGN KEY(func_id_funcionario) REFERENCES funcionario (id_funcionario)
+);
+
+CREATE TABLE obra (
+cod_obra int PRIMARY KEY,
+ano_obra year not null,
+titu_obra varchar(80) not null unique,
+peso_obra decimal(10,2) null,
+material_obra varchar(80) null,
+desc_estilo_obra varchar(80) null,
+cod_autor int not null,
+cod_tipo_obra int not null,
+FOREIGN KEY(cod_autor) REFERENCES autor (cod_autor)
+);
+
+CREATE TABLE tipo_obra (
+cod_tipo_obra int PRIMARY KEY,
+desc_tipo_obra varchar(80) not null
+);
+
+CREATE TABLE manu_mat (
+Campo_1 int not null,
+Campo_2 int not null,
+qtd_mat_mnt varchar(15) not null,
+FOREIGN KEY(Campo_1) REFERENCES manutencao (mnt_obra),
+FOREIGN KEY(Campo_2) REFERENCES materia_prima (cod_mat_prima)
+);
+
+ALTER TABLE salao_obra ADD FOREIGN KEY(obra_cod_obra) REFERENCES obra (cod_obra);
+ALTER TABLE atividade ADD FOREIGN KEY(ob_cod_obra) REFERENCES obra (cod_obra);
+ALTER TABLE atividade ADD FOREIGN KEY(func_id_funcionario) REFERENCES funcionario (id_funcionario);
+ALTER TABLE manutencao ADD FOREIGN KEY(cod_obra) REFERENCES obra (cod_obra);
+ALTER TABLE obra ADD FOREIGN KEY(cod_tipo_obra) REFERENCES tipo_obra (cod_tipo_obra);
